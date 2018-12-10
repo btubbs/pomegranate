edbe3385357098c33fe4c0e1153ac6367fe2118e
Brent Tubbs <brent.tubbs@gmail.com>
2018-12-10 14:19:18 -0700

    v0.0.6

--- pmg/pmg.go
+++ pmg/pmg.go
@@ -17,7 +17,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "pmg"
 	app.Usage = "Create and run Postgres migrations"
-	app.Version = "0.0.3"
+	app.Version = "0.0.6"
 
 	// dirFlag and dbFlag are declared once up here and used in multiple places
 	// below.  Single-use flags will be declared inline.
